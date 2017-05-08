ffcaeafb9bcc9ef0384e8585a979b0c671e340de
Carlos Ledesma <carlosledesma@clc.local>
2017-05-08 20:08:35 +0200

    Añadido texto al readme y jscscr

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var anuncios = require('./routes/anuncios');
+
 var app = express();
 
 // view engine setup
@@ -18,7 +19,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Práctica Nodepop
+
+Backend para una tienda de busqueda y venta de productos de 2ª mano.
+
+## Requisitos
+
+Node 7.10.0
+Express 4.15.2
+MongoDb 3.4.4
+
+## Instalación
