a160dfca468878de1e92d6d40849ccb4b2053860
Carlos Ledesma <carlosledesma@clc.local>
2017-05-11 18:09:50 +0200

    Terminada 1ª versión de script de borrado y escritura en db

--- bin/install_db.js
+++ bin/install_db.js
@@ -1,40 +1,62 @@
 'use strict';
 
-require('../lib/connectionDb');
+// Cargamos los modelos
+require('../models/Anuncio');
+require('../models/Usuario');
 
-const async = require('async');
+// Cargamos módulos que vamos a necesitar en la instalación
+const sha = require('sha256');
 const mongoose = require('mongoose');
+const Anuncio = mongoose.model('Anuncio');
+const Usuario = mongoose.model('Usuario');
+const fs = require('fs');
+const json = JSON.parse(fs.readFileSync('bin/anuncios.json',{ encoding: 'utf-8'}));
 
-async.series({
-    borrarTablaUsuarios: (cb) => {
-        cb(null, 'OK');
-      },
-    borrarTablaAnuncios: (cb) => {
-        cb(null, 'OK');
-      },
-    cargarAnuncios: (cb) => {
-        cb(null, 'OK');
-      },
-    crearUsuario: (cb) => {
-        cb('bu', 'NO-OK');
-      },
-  }, (err, results) => {
-    if (err) {
-      console.log(err);
-      // Aprocess.exit(1);
-    }
-    consolify(results);
-    process.exit(0);
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.log(err);
   });
 
-function consolify(data) {
-  const keys = Object.keys(data);
-  let color = '';
-  for (var i = 0; i < keys.length; i++) {
-    if (data[keys[i]] === 'OK') {
-      console.log(keys[i],' [','\x1b[32m',data[keys[i]],'\x1b[0m',']');
-    } else {
-      console.log(keys[i],' [','\x1b[31m',data[keys[i]],'\x1b[0m',']');
-    }
+
+db.once('open', () => {
+    console.log('<------------------>');
+    console.log('Conectado a mongodb');
+  });
+
+mongoose.connect('mongodb://localhost/nodepop',(err)=> {
+  if (err) {
+    console.log(err);
+    return;
   }
-}
+  Anuncio.remove({}, (err, algo) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Borrado de anuncios completado');
+    Usuario.remove({}, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Borrado de usuarios completado');
+      Anuncio.insertMany(json.anuncios,(err,algo) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Inserción de anuncios de prueba realizado con éxito');
+        let pass = sha('hinkliminkli');
+        Usuario.create({ nombre: 'Carlos', email: 'clc@clc.es', clave: pass},(err, usuario) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log('Usuario de prueba insertado con éxito');
+          mongoose.connection.close();
+          process.exit(0);
+        })
+      });
+    });
+  });
+});
