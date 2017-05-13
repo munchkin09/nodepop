3562e32246b347c4c8d6b3a6d2bf7cc38663ce95
Carlos Ledesma <carlos.developing@gmail.com>
2017-05-13 15:56:13 +0200

    Añadida lista de usuarios en petición GET de /api/v1/usuarios

--- routes/api/v1/usuarios.js
+++ routes/api/v1/usuarios.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET users listing. */
+const Usuario = mongoose.model('Usuario');
+
+/* GET lista de usuarios. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Usuario.find({}, 'nombre email', (err, data) => {
+    if (err) {
+      next(err,null);
+      return;
+    }
+    res.json({success: true, data: data });
+  });
 });
 
 
