4272493d9867bb84fb223f1b8a1cac9276773cef
Carlos Ledesma <carlos.developing@gmail.com>
2017-05-13 13:33:46 +0200

    Nuevo metodo para encontrar usuario por mail y clave

--- models/usuario.js
+++ models/usuario.js
@@ -1,11 +1,34 @@
 'use strict';
 
 var mongoose = require('mongoose');
+const sha = require('sha256');
 
-var usuarioSchema = mongoose.Schema({
+const usuarioSchema = mongoose.Schema({
     nombre: String,
     email: String,
     clave: String,
   });
 
-mongoose.model('Usuario', usuarioSchema);
+usuarioSchema.statics.borrarUsuarios = function(cb) {
+  Usuario.remove({},cb);
+}
+
+usuarioSchema.statics.insertarUsuario = function(data, cb) {
+  const pass = sha(data.clave);
+  data.clave = pass;
+  new Usuario(data).save(cb);
+}
+
+usuarioSchema.statics.validarUsuarioYPass = function(data, cb) {
+  const login = { email: data.email, clave: sha(data.clave) }
+  Usuario.findOne(login, (err, data) => {
+    if (err) {
+      cb(err, null);
+    }
+    cb(null, data);
+  });
+}
+
+var Usuario = mongoose.model('Usuario', usuarioSchema);
+
+// Mmodule.exports = Usuario;
